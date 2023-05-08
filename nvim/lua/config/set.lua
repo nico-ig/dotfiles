aa8557db0141867500ce97e4ce946cde655dba9d
Nico Ramos <nicola.ramos@exati.com.br>
2023-05-08 08:57:53 -0300

    add nvim dotfiles

---
+++ nvim/lua/config/set.lua
@@ -0,0 +1,87 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Enable relative numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.smartindent = true
+
+-- Set tab to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Set an 80 column border
+vim.o.cc = 80
+
+-- Highlith current cursorline
+vim.o.cursorline = true
+
+-- Speed up scrolling
+vim.o.ttyfast = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Middle-click paste with
+vim.o.mouse = 'v'
+
+-- Undotree responsible for the undo files
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
