4b55a834b32d332add659a879d87e685a9c4551c
Nico <nico.igramos@gmail.com>
2025-02-18 18:55:15 -0300

    NVIM -- [PATCH] -- Change indent lines style

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "HiPhish/rainbow-delimiters.nvim",
-    config = function()
-      require('rainbow-delimiters.setup').setup({})
-    end
-  },
-  {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {},
@@ -19,6 +13,7 @@ return {
         "RainbowViolet",
         "RainbowCyan",
       }
+
       local hooks = require "ibl.hooks"
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
@@ -32,10 +27,7 @@ return {
         vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
       end)
 
-      vim.g.rainbow_delimiters = { highlight = highlight }
-      require("ibl").setup { scope = { highlight = highlight, char = "┆" } }
-
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+      require("ibl").setup { indent = { highlight = highlight, char = "┊" } }
     end
   }
 }
