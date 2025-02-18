2f2aa35a820576805a88a61f8b3bedf8b2a85c3a
Nico <nico.igramos@gmail.com>
2025-02-18 18:54:21 -0300

    NVIM -- [FIX] -- Fix notifications

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -30,6 +30,8 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  change_detection = {
+    enabled = false,
+    notify = false,
+  }
 })
