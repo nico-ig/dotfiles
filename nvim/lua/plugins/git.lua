1245aef632d09c68b9a40a947d0720cbd0ceb795
Nico <nico.igramos@gmail.com>
2025-02-18 18:54:51 -0300

    NVIM -- [PATCH] -- Changes git signs

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,7 +3,18 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup({})
+      require("gitsigns").setup({
+        signs = {
+          add = { text = '+' },
+          change = { text = '∾' },
+          untracked = { text = '│' }
+        },
+        signs_staged = {
+          add = { text = '+' },
+          change = { text = '∾' },
+          untracked = { text = '│' }
+        }
+      })
     end
   }
 }
