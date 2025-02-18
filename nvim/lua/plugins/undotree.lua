88d6eddff466551f24d9f375e5292655adc92610
Nico <nico.igramos@gmail.com>
2025-02-18 18:55:33 -0300

    NVIM -- [FIX] -- Fix undotree not persisting

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,8 +1,13 @@
 return {
-  "jiaoshijie/undotree",
-  dependencies = "nvim-lua/plenary.nvim",
-  config = true,
-  keys = {
-    { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
-  },
+  "mbbill/undotree",
+  config = function ()
+    vim.o.undofile = true
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    vim.g.undotree_WindowLayout = 2
+    vim.g.undotree_ShortIndicators = 1
+    vim.g.undotree_SplitWidth = vim.fn.winwidth(0) - 105
+    vim.g.undotree_DiffpanelHeight = vim.fn.winheight(0) / 3
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_DiffCommand = "diff"
+  end,
 }
